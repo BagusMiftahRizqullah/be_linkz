41ecef08241dea9e3486d81e9dcc0b66ac3bcc0f
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2024-04-18 16:57:00 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.use(cors());
+  await app.use(bodyParser.urlencoded({ extended: false }));
+  await app.use(bodyParser.json({ limit: '50mb' }));
+  await app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+    }),
+  );
+  await app.listen(3333);
 }
 bootstrap();
